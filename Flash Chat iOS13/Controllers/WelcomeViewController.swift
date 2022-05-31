dcc7e2fea3cea37683f95129909f3f27f487a77f
Alessa <alosherto@gmail.com>
2022-05-31 12:23:00 -0400

    3 commit

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,11 +12,21 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        //loop for FlashCaht animation.
+        titleLabel.text = ""
+        var characterIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print("-")
+            print(01 * characterIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            characterIndex += 1
+        }
     }
-    
-
 }
